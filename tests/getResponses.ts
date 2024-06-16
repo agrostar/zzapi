ebdffad85a54a5981797cb227d93add3d2ed69c1
Varun0157 <varun.edachali@gmail.com>
2024-06-17 01:27:27 +0530

    feat: pre-processing of test requests

---
+++ tests/getResponses.ts
@@ -0,0 +1,9 @@
+import { RequestSpec, ResponseData } from "../src";
+import { RawRequest } from "./utils/requestUtils";
+
+export async function runRequestTests(
+  requests: { [name: string]: RequestSpec },
+  rawReq: RawRequest,
+): Promise<void> {
+    
+}
