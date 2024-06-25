4abc40bd610dd3f912ff890ee818966bdec9c7fc
Varun0157 <varun.edachali@gmail.com>
2024-06-26 04:08:08 +0530

    refactor: moved tests to a separate dictionary for clarity

--- src/runTests.ts
+++ src/runTests.ts
@@ -86,177 +86,275 @@ function getValueForJSONTests(responseContent: object, key: string): any {
   }
 }
 
+function getType(data: any): string {
+  if (data === null) return "null";
+  if (Array.isArray(data)) return "array";
+  return typeof data;
+}
+
 const SKIP_CLAUSE = "$skip",
-  OPTIONS_CLAUSE = "$options";
+  OPTIONS_CLAUSE: string = "$options";
 
-function runObjectTests(
-  opVals: { [key: string]: any },
-  receivedObject: any,
-  spec: string,
-  skip?: boolean,
-): SpecResult {
-  let objRes: SpecResult = { spec, results: [], subResults: [] };
-  if (skip || opVals[SKIP_CLAUSE]) objRes.skipped = true;
-
-  for (const op in opVals) {
-    let expected: Exclude<any, object> = getStringIfNotScalar(opVals[op]);
-    let received: Exclude<any, object> = getStringIfNotScalar(receivedObject);
-
-    let pass = false;
-    let message = "";
-    if (op === "$eq") {
-      pass = received === expected;
-    } else if (op === "$ne") {
-      pass = received !== expected;
-    } else if (op === "$lt") {
-      pass = received < expected;
-    } else if (op === "$gt") {
-      pass = receivedObject > expected;
-    } else if (op === "$lte") {
-      pass = receivedObject <= expected;
-    } else if (op === "$gte") {
-      pass = receivedObject >= expected;
-    } else if (op === "$size") {
-      const res: SpecResult = testSize(opVals[op], receivedObject, spec, op);
-      objRes.results.push(...res.results);
-      objRes.subResults.push(...res.subResults);
-      continue;
-    } else if (op === "$exists") {
-      const exists = received !== undefined;
-      pass = exists === expected;
-    } else if (op === "$type") {
-      const receivedType = getType(receivedObject);
-      pass = expected === receivedType;
-      received = `${received} (type ${receivedType})`;
-    } else if (op === "$regex") {
-      const options = opVals[OPTIONS_CLAUSE];
-      const regex = new RegExp(expected, options);
-      try {
-        pass = typeof received === "string" && regex.test(received);
-      } catch (err: any) {
-        message = err.message;
-      }
-    } else if (op === "$sw") {
-      pass = typeof received === "string" && received.startsWith(expected);
-    } else if (op === "$ew") {
-      pass = typeof received === "string" && received.endsWith(expected);
-    } else if (op === "$co") {
-      pass = typeof received === "string" && received.includes(expected);
-    } else if (op === OPTIONS_CLAUSE) {
-      continue; // do nothing. $regex will address it.
-    } else if (op === "$tests") {
-      const res: SpecResult = testRecursiveTests(
-        opVals[op],
-        receivedObject,
-        spec,
-        op,
-        objRes.skipped ?? false,
-      );
-      objRes.results.push(...res.results);
-      objRes.subResults.push(...res.subResults);
-      continue;
-    } else if (op === SKIP_CLAUSE) {
-      continue; // do nothing. If it wasn't already addressed, that means the test is not to be skipped.
-    } else {
-      objRes.results.push({
-        pass: false,
-        expected: "one of $eq, $ne etc.",
-        received: op,
-        op: "",
-        message: "To compare objects, use $eq",
-      });
-      continue;
-    }
-    objRes.results.push({ pass, expected, received, op, message });
-  }
-
-  return objRes;
-}
-
-function testSize(expectedObject: any, receivedObject: any, spec: string, op: string): SpecResult {
-  const res: SpecResult = { spec, results: [], subResults: [] };
-
-  const receivedLen: number | undefined =
-    typeof receivedObject === "object" && receivedObject !== null
-      ? Object.keys(receivedObject).length
-      : typeof receivedObject === "string" || Array.isArray(receivedObject)
-        ? receivedObject.length
-        : undefined;
-
-  const received: Exclude<any, object> = getStringIfNotScalar(receivedObject);
-  const expected: Exclude<any, object> = getStringIfNotScalar(expectedObject);
-
-  if (typeof expectedObject === "number") {
-    const compResult: TestResult = {
-      pass: expected === receivedLen,
-      op: op,
-      expected: expected,
-      received: received,
+const tests: {
+  [name: string]: (
+    expectedObj: any,
+    receivedObj: any,
+    spec: string,
+    op: string,
+    options: { [key: string]: any },
+  ) => SpecResult;
+} = {
+  $eq: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: received === expected, expected, received, op }],
     };
-    res.results.push(compResult);
+  },
+  $ne: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: received !== expected, expected, received, op }],
+    };
+  },
+  $gt: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: received > expected, expected, received, op }],
+    };
+  },
+  $lt: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: received < expected, expected, received, op }],
+    };
+  },
+  $lte: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: received <= expected, expected, received, op }],
+    };
+  },
+  $gte: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: received >= expected, expected, received, op }],
+    };
+  },
+  $size: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const res: SpecResult = { spec, results: [], subResults: [] };
 
-    return res;
-  }
+    const receivedLen: number | undefined =
+      typeof receivedObj === "object" && receivedObj !== null
+        ? Object.keys(receivedObj).length
+        : typeof receivedObj === "string" || Array.isArray(receivedObj)
+          ? receivedObj.length
+          : undefined;
 
-  if (isDict(expectedObject)) {
-    // the spec remains the same, so we add it to the current layer
-    const compRes: SpecResult = runObjectTests(expectedObject, receivedLen, spec);
-    res.results.push(...compRes.results);
-    res.subResults.push(...compRes.subResults);
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj);
+    const expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
 
-    return res;
-  }
+    if (typeof expectedObj === "number") {
+      const compResult: TestResult = {
+        pass: expected === receivedLen,
+        op: op,
+        expected: expected,
+        received: received,
+      };
+      res.results.push(compResult);
 
-  const compResult: TestResult = {
-    pass: false,
-    op: op,
-    expected: expected,
-    received: received,
-    message: "value for $size is not a number or valid JSON",
-  };
-  res.results.push(compResult);
+      return res;
+    }
 
-  return res;
-}
+    if (isDict(expectedObj)) {
+      // the spec remains the same, so we add it to the current layer
+      const compRes: SpecResult = runObjectTests(expectedObj, receivedLen, spec);
+      res.results.push(...compRes.results);
+      res.subResults.push(...compRes.subResults);
 
-function testRecursiveTests(
-  expectedObject: any,
-  receivedObject: any,
-  spec: string,
-  op: string,
-  skip: boolean,
-): SpecResult {
-  const res: SpecResult = { spec, results: [], subResults: [] };
+      return res;
+    }
 
-  const expected: Exclude<any, object> = getStringIfNotScalar(expectedObject);
-  const received: Exclude<any, object> = getStringIfNotScalar(receivedObject);
-
-  const recursiveTests = expectedObject;
-  if (!isDict(recursiveTests)) {
     const compResult: TestResult = {
       pass: false,
       op: op,
       expected: expected,
       received: received,
-      message: "recursive tests must be dicts",
+      message: "value for $size is not a number or valid JSON",
     };
     res.results.push(compResult);
 
     return res;
+  },
+  $exists: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    const exists = received !== undefined;
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: exists === expected, expected, received, op }],
+    };
+  },
+  $type: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const receivedType = getType(receivedObj);
+    const receivedStr: string = `${getStringIfNotScalar(receivedObj)} (type ${receivedType})`;
+    const expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass: expected === receivedType, expected, received: receivedStr, op }],
+    };
+  },
+  $regex: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+
+    const regexOpts = options[OPTIONS_CLAUSE];
+    const regex = new RegExp(expected, regexOpts);
+    let pass: boolean = false,
+      message: string = "";
+    try {
+      pass = typeof received === "string" && regex.test(received);
+    } catch (err: any) {
+      message = err.message;
+    }
+
+    return {
+      spec,
+      subResults: [],
+      results: [{ pass, expected, received, op, message }],
+    };
+  },
+  $sw: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [
+        { pass: typeof received === "string" && received.startsWith(expected), expected, received, op },
+      ],
+    };
+  },
+  $ew: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [
+        { pass: typeof received === "string" && received.endsWith(expected), expected, received, op },
+      ],
+    };
+  },
+  $co: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj),
+      expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    return {
+      spec,
+      subResults: [],
+      results: [
+        { pass: typeof received === "string" && received.includes(expected), expected, received, op },
+      ],
+    };
+  },
+  [OPTIONS_CLAUSE]: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    return {
+      spec,
+      subResults: [],
+      results: [],
+    };
+  },
+  [SKIP_CLAUSE]: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    return {
+      spec,
+      subResults: [],
+      results: [],
+    };
+  },
+  $tests: function (expectedObj, receivedObj, spec, op, options): SpecResult {
+    const res: SpecResult = { spec, results: [], subResults: [] };
+
+    const expected: Exclude<any, object> = getStringIfNotScalar(expectedObj);
+    const received: Exclude<any, object> = getStringIfNotScalar(receivedObj);
+
+    const recursiveTests = expectedObj;
+    if (!isDict(recursiveTests)) {
+      const compResult: TestResult = {
+        pass: false,
+        op: op,
+        expected: expected,
+        received: received,
+        message: "recursive tests must be dicts",
+      };
+      res.results.push(compResult);
+
+      return res;
+    }
+
+    mergePrefixBasedTests(recursiveTests);
+
+    // the spec remains the same, so we add it to the current layer
+    const compRes = runAllTests(recursiveTests, receivedObj, false, spec, options[SKIP_CLAUSE]);
+    res.results.push(...compRes.results);
+    res.subResults.push(...compRes.subResults);
+
+    return res;
+  },
+};
+
+export function runObjectTests(
+  opVals: { [key: string]: any },
+  receivedObject: any,
+  spec: string,
+  skip?: boolean,
+): SpecResult {
+  const objRes: SpecResult = {
+    spec,
+    results: [],
+    subResults: [],
+    skipped: skip || opVals[SKIP_CLAUSE],
+  };
+
+  const options: { [key: string]: any } = {
+    [OPTIONS_CLAUSE]: opVals[OPTIONS_CLAUSE],
+    [SKIP_CLAUSE]: objRes.skipped,
+  };
+
+  const testNames = Object.keys(tests);
+  for (const op in opVals) {
+    if (testNames.includes(op)) {
+      const res: SpecResult = tests[op](opVals[op], receivedObject, spec, op, options);
+      objRes.results.push(...res.results);
+      objRes.subResults.push(...res.subResults);
+    } else {
+      objRes.results.push({
+        pass: false,
+        expected: `one of ${testNames.join(", ")}`,
+        received: op,
+        op: "",
+        message: "Note: use $eq to compare objects",
+      });
+    }
   }
 
-  mergePrefixBasedTests(recursiveTests);
-
-  // the spec remains the same, so we add it to the current layer
-  const compRes = runObjectTests(recursiveTests.json, receivedObject, spec, skip);
-  res.results.push(...compRes.results);
-  res.subResults.push(...compRes.subResults);
-
-  return res;
-}
-
-function getType(data: any): string {
-  if (data === null) return "null";
-  if (Array.isArray(data)) return "array";
-  return typeof data;
+  return objRes;
 }
