0ee1862db7109a253870814e029ed4ab6a611077
Varun0157 <varun.edachali@gmail.com>
2024-06-30 07:27:27 +0530

    refactor: $skip -> skip

--- tests/runTests.ts
+++ tests/runTests.ts
@@ -113,9 +113,9 @@ function allPositive(res: SpecResult, numTests: number): string[] {
   return errors;
 }
 
-const SKIP_CLAUSE = "$skip";
+const SKIP_CLAUSE = "skip";
 const TEST_CLAUSE = "$tests";
-const NON_TEST_KEYS = ["$options", "multi", TEST_CLAUSE, SKIP_CLAUSE];
+const NON_TEST_KEYS = ["options", "multi", TEST_CLAUSE, SKIP_CLAUSE];
 
 function getNumTests(tests: Tests) {
   let numTests = 0;
